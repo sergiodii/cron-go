0fdd116903ce7fa23b593aca65b66eb22f39006b
AlexandreJunior <sergio.silva@mercadofavo.com>
2021-08-10 03:27:41 -0300

    start cron

--- cron/main_cron.go
+++ cron/main_cron.go
@@ -3,11 +3,9 @@ package main_cron
 import (
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/robfig/cron"
 	cron_services "github.com/sergiodii/cron-go/cron/src/services"
-	cron_utils "github.com/sergiodii/cron-go/cron/src/utils"
 )
 
 func Main() {
@@ -16,21 +14,21 @@ func Main() {
 	c := cron.New()
 	cron_services.ExecuteCrons(&cronsRunningList, c)
 
-	time.AfterFunc(10*time.Minute, func() {
-		exChan := make(chan int)
-		go func(ch chan int) {
-			cron_services.SyncJobs()
-			cron_services.ExecuteCrons(&cronsRunningList, c)
-			ch <- 1
-		}(exChan)
-		<-exChan
-	})
+	// time.AfterFunc(10*time.Minute, func() {
+	// 	cron_services.SyncJobs()
+	// 	cron_services.ExecuteCrons(&cronsRunningList, c)
+	// 	// exChan := make(chan int)
+	// 	// go func(ch chan int) {
+	// 	// 	ch <- 1
+	// 	// }(exChan)
+	// 	// <-exChan
+	// })
 
 	go c.Start()
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt, os.Kill)
-
-	cron_utils.Logger.Fatal("CRON-GO-CRON EXIT:", <-sig)
+	<-sig
+	// cron_utils.Logger.Fatal("CRON-GO-CRON EXIT:", <-sig)
 
 }
 
